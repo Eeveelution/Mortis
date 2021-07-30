636e3bdbdd4f6dd4963941424fcd6d3090ba2ce6
Eeveelution <developerbrzoza@gmail.com>
2021-07-30 11:32:51 +0200

    Base Projects

---
+++ Mortis.Bancho.Serializer/Class1.cs
@@ -0,0 +1,5 @@
+﻿using System;
+
+namespace Mortis.Bancho.Serializer {
+    public class Class1 {}
+}
---
+++ Mortis.Bancho/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Mortis.Bancho {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
